f140ffae1a4a93af5a8da41467525be0a665247f
djo1975 <mladanilic1975@gmail.com>
2023-03-06 15:50:00 +0100

    Linter errors fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-import booksSlice from './booksSlice';
-import categoriesSlice from './categoriesSlice';
+import booksSlice from '../components/BooksSlices';
+import categoriesSlice from '../categories/categoriesSlice';
 
 const store = configureStore({
   reducer: {
